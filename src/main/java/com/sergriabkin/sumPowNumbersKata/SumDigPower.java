fb72732d73d86d4d722441cf550f3a34d92974d9
scor92 <sergriabkin@gmail.com>
2019-11-23 17:25:17 +0200

    someDigPower kata start

---
+++ src/main/java/com/sergriabkin/sumPowNumbersKata/SumDigPower.java
@@ -0,0 +1,8 @@
+package com.sergriabkin.sumPowNumbersKata;
+
+import java.util.Collections;
+import java.util.List;
+
+public class SumDigPower {
+    public static List<Long> sumDigPow(long a, long b) {return Collections.emptyList();}
+}
