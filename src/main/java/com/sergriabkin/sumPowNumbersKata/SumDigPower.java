dff1b244cf4510584d1cdb1f2705ec41bbda1bbb
scor92 <sergriabkin@gmail.com>
2019-11-23 21:54:32 +0200

    refactoring

--- src/main/java/com/sergriabkin/sumPowNumbersKata/SumDigPower.java
+++ src/main/java/com/sergriabkin/sumPowNumbersKata/SumDigPower.java
@@ -20,23 +20,22 @@ public class SumDigPower {
 
     private static long getSumOfPowers(List<Long> digits) {
         return IntStream.range(0, digits.size())
-                .mapToLong(i -> calculateNumberInPower(digits.get(i), i+1))
-
+                .mapToLong(i -> calculateNumberInPower(digits.get(i), i + 1))
                 .sum();
     }
 
     private static long calculateNumberInPower(long number, int timesToMultiply) {
         long result = 1;
         for (int i = 0; i < timesToMultiply; i++) {
-            result = number * result;
+            result *= number;
         }
         return result;
     }
 
     private static List<Long> getDigitsListFromNumber(long number) {
-        char[] chars = ("" + number).toCharArray();
-        return IntStream.range(0, chars.length)
-                .mapToObj(i-> ""+chars[i])
+        String chars = "" + number;
+        return IntStream.range(0, chars.length())
+                .mapToObj(i -> "" + chars.charAt(i))
                 .map(Long::valueOf)
                 .collect(Collectors.toList());
     }
